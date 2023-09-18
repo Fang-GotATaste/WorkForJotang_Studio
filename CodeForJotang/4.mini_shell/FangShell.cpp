fe6e9383098e1c8563b5bb3311957b1fc680c2d3
Fang-GotATaste <aphextwin3@loutlook.com>
2023-09-18 12:59:30 +0800

    新文件：   .vscode/c_cpp_properties.json 	修改：     "CodeForJotang/3.linux/\350\277\207\347\250\213.md"     新文件：   CodeForJotang/4.mini_shell/FangShell.cpp 	新文件：   CodeForJotang/4.mini_shell/Readme.md 	修改：     "CodeForJotang/4.mini_shell/\350\277\207\347\250\213.md"

---
+++ CodeForJotang/4.mini_shell/FangShell.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<string> 
+#include<unistd.h>
+
+std::string read()//read
+{
+
+}
+
+std::string parse()
+{
+
+}
+
+bool execute(std::string command)
+{
+
+}
+
+void loop()//  basic lifetime
+{
+    //keep receiving and spliting command string  
+    do//keep(?) the prompt while active
+    {
+        std::string prompt="";
+    } while (/*activate*/);
+    
+    
+}
+
+
+int main()
+{
+    //initialize
+    loop();
+    //quit
+    return 0;
+}
---
+++ CodeForJotang/4.mini_shell/Readme.md
@@ -0,0 +1,3 @@
+This project named FangShell is bulit as a imitation of a mini-Shell in "https://brennan.io/2015/01/16/write-a-shell-in-c/" and "https://www.bilibili.com/video/BV17W4y1H7GC/?spm_id_from=333.337.search-card.all.click&vd_source=91d1c47a53010e103a5d463866fa18ac"<br />
+This project works on Linux<br />
+Type "help" for a list of useable command<br />
