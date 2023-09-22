893bd00a225b60cdf8325a742e58d257eab5ffc1
Fang-GotATaste <134762453+Fang-GotATaste@users.noreply.github.com>
2023-09-22 23:26:22 +0800

    modified:   .vscode/settings.json 	modified:   CodeForJotang/4.mini_shell/FangShell.c 	modified:   "CodeForJotang/4.mini_shell/\350\277\207\347\250\213.md"

--- CodeForJotang/4.mini_shell/FangShell.c
+++ CodeForJotang/4.mini_shell/FangShell.c
@@ -1,1 +1,40 @@
-#include<>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+string commandlist[] = {"cd", "help", "exit", "alias", "kill", "pwd"};
+int (*commandtofind[])()=
+{
+    &cd,&help,&exit,&alias,&kill,&pwd
+}
+{
+}
+int cd()
+{
+}
+int help()
+{
+}
+int exit()
+{
+}
+int alias()
+{
+}
+int kill()
+{
+}
+int pwd()
+{
+}
+void loop()
+{   
+    
+}
+
+
+int main()
+{
+    void loop();
+    return 0;
+}
