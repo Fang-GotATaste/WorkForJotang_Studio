c0feece1c2f4fc78f6f7838763477b5a3a44a188
Fang-GotATaste <134762453+Fang-GotATaste@users.noreply.github.com>
2023-10-06 01:03:25 +0800

    modified:   .vscode/settings.json 	modified:   CodeForJotang/4.mini_shell/FangShell.c 	modified:   "CodeForJotang/4.mini_shell/\350\277\207\347\250\213.md"

--- CodeForJotang/4.mini_shell/FangShell.c
+++ CodeForJotang/4.mini_shell/FangShell.c
@@ -1,12 +1,14 @@
-#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-
-string commandlist[] = {"cd", "help", "exit", "alias", "kill", "pwd"};
-int (*commandtofind[])()=
-{
-    &cd,&help,&exit,&alias,&kill,&pwd
-}
+commandlist[] = {"cd", "help", "exit", "alias", "kill", "pwd"};
+int (*commandtofind[])() =
+    {
+        &cd, &help, &exit, &alias, &kill, &pwd}
 {
 }
 int cd()
@@ -27,14 +29,38 @@ int kill()
 int pwd()
 {
 }
-void loop()
-{   
-    
-}
 
+void loop()
+{
+    int STATUS;
+    do
+    {
+        // 自定义命令提示符
+        char prompt[] = "[FangShell]" + getpwuid((getuid())) + '@' + gethostname() + ':' + getcwd() + (geteuid() == 0 ? '#' : '$' +);
+        printf("%s", prompt);
+
+        /*原教程的输入逻辑
+        fflush(stdin);
+        fgets(cmdin, 256, stdin);*/
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF)
+            ;
+        fgets(cmdin, 256, stdin);
+
+        int i;
+        for (i = 0; cmd[i] != '\n'; i++)
+            ;
+        cmd[i] = '\0'; // 去掉读入的换行符
+
+        args[0] = cmd;
+        
+    } while (STATUS);
+}
 
 int main()
 {
+    char cmdin[256];
+    char *args[128];
     void loop();
     return 0;
 }
